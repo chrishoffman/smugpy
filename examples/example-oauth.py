77ceba254945ea8cd52bc16dfe836364b2915491
Chris Hoffman <christopher.hoffman@gmail.com>
2011-04-30 11:16:59 -0400

    Adding oauth example

---
+++ examples/example-oauth.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python
+
+from smugpy import SmugMug
+
+API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXX"
+OAUTH_SECRET = "YYYYYYYYYYYYYYYYYYYYYYY"
+
+smugmug = SmugMug(api_key=API_KEY, oauth_secret=OAUTH_SECRET)
+
+#Oauth handshake
+smugmug.auth_getRequestToken()
+raw_input("Authorize app at %s\n\nPress Enter when complete.\n" % (smugmug.authorize()))   
+smugmug.auth_getAccessToken()
+
+albums = smugmug.albums_get(NickName="williams") # Moon River Photography, thanks Andy!
+for album in albums["Albums"]:
+    print "%s, %s" % (album["id"], album["Title"])
