373166d8d371331cdc008692afb56db29cf911f6
Chris Hoffman <christopher.hoffman@gmail.com>
2012-07-02 22:52:02 -0400

    Packaging updates

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import distutils.core
+from distutils.core import setup
 import sys
 from textwrap import dedent
 
@@ -6,7 +6,7 @@ install_requires = []
 if sys.version_info < (2, 6):
     install_requires.append("simplejson")
 
-distutils.core.setup(
+setup(
     name = "smugpy",
     version = "0.3.0-pre",
     description = "SmugMug Python API",
@@ -16,6 +16,21 @@ distutils.core.setup(
     url = "http://github.com/chrishoffman/smugpy/",
     platforms = ["any"],
     packages = ["smugpy"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     install_requires = install_requires,
     long_description = dedent("""\
     Python SmugMug Helper Library
