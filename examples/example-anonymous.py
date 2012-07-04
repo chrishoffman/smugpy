97d691ff9e47c080852e10a5c1de9b291c9e05b2
Chris Hoffman <christopher.hoffman@gmail.com>
2012-07-04 14:35:05 -0400

    Forcing api version, in case default changes

--- examples/example-anonymous.py
+++ examples/example-anonymous.py
@@ -5,7 +5,7 @@ from smugpy import SmugMug
 
 API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXX"
 
-smugmug = SmugMug(api_key=API_KEY, app_name="TestApp")
+smugmug = SmugMug(api_key=API_KEY, api_version="1.2.2", app_name="TestApp")
 smugmug.login_anonymously()
 albums = smugmug.albums_get(NickName="williams") # Moon River Photography, thanks Andy!
 
